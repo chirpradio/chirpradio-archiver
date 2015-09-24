chirpradio/chirpradio-archiver

commit 01916f5a9eb69aa5dffce91993ea4da4e8f53f3f

panic about file IO errors

archiver.go
@@ -178,11 +178,11 @@ func NewArchiveFileWriter(
 	return &ArchiveFileWriter{broadcast, quit, fileName}
 }
 
-func writeArchiveFile(writer ArchiveWriter) error {
+func writeArchiveFile(writer ArchiveWriter) {
 	output, err := writer.OpenFile()
 	if err != nil {
 		log("Error while creating", writer.FileName(), ":", err)
-		return err
+		panic(err)
 	}
 
 	for {
@@ -191,7 +191,6 @@ func writeArchiveFile(writer ArchiveWriter) error {
 			output.Write(streamChunk)
 		case <-writer.Quit():
 			output.Close()
-			return nil
 		}
 	}
 }
@@ -200,7 +199,7 @@ func writeArchiveFile(writer ArchiveWriter) error {
 type ArchiveConfig interface {
 	FileName(dest string, ts time.Time) string
 	Dest(ts time.Time) string
-	WriteFile(writer ArchiveWriter) error
+	WriteFile(writer ArchiveWriter)
 }
 
 type ChirpArchiveConfig struct {
@@ -226,9 +225,9 @@ func (*ChirpArchiveConfig) FileName(dest string, ts time.Time) string {
 	)
 }
 
-func (archive *ChirpArchiveConfig) WriteFile(writer ArchiveWriter) error {
+func (archive *ChirpArchiveConfig) WriteFile(writer ArchiveWriter) {
 	// TODO: maybe move the writeArchiveFile implementation over here :)
-	return writeArchiveFile(writer)
+	writeArchiveFile(writer)
 }
 
 func NewChirpArchiveConfig(rootDir string) (*ChirpArchiveConfig) {

archiver_test.go
@@ -29,16 +29,16 @@ type MockArchiveConfig struct {
 	writerCalled chan bool
 }
 
-func (archive *MockArchiveConfig) WriteFile(info ArchiveWriter) error {
+func (archive *MockArchiveConfig) WriteFile(info ArchiveWriter) {
 	archive.writerCalled <- true
 	close(archive.writerCalled)
-	return nil
 }
 
 func NewMockArchiveConfig(
 		writerCalled chan bool, rootDir string) *MockArchiveConfig {
 	chirpConfig := NewChirpArchiveConfig(rootDir)
-	return &MockArchiveConfig{ChirpArchiveConfig: chirpConfig, writerCalled: writerCalled}
+	return &MockArchiveConfig{
+		ChirpArchiveConfig: chirpConfig, writerCalled: writerCalled}
 }
 
 func TestRotateArchiveFile(t *testing.T) {
@@ -224,11 +224,17 @@ func TestWriteArchiveFileWithError(t *testing.T) {
 	// expected.
 	close(writer.Quit())
 
-	result := writeArchiveFile(writer)
+	recovered := false
+	defer func() {
+		if r := recover(); r != nil {
+			recovered = true
+		}
+		if !recovered {
+			t.Error("Expected to recover from a panic")
+		}
+	}()
 
-	if result == nil {
-		t.Error("Unexpected result")
-	}
+	writeArchiveFile(writer)
 }
 
 

commit 18e02a3cd58003a987cd6bdc82ca6c4cdfd1b870

Fixed regression where writer channel never exited
Ha! The archiver process was eating 100% cpu and dtruss said it was selecting like a madman. This was a regression from
https://github.com/chirpradio/chirpradio-archiver/commit/01916f5a9eb69aa5dffce91993ea4da4e8f53f3f#diff-635e4219ee55ef011b2b32bba065606bL194

archiver.go
@@ -221,6 +221,7 @@ func (archive *ChirpArchiveConfig) WriteFile(writer ArchiveWriter) {
 			output.Write(streamChunk)
 		case <-writer.Quit():
 			output.Close()
+			return
 		}
 	}
 }

archiver_test.go
@@ -207,6 +207,28 @@ func TestWriteArchiveFile(t *testing.T) {
 }
 
 
+func TestWriteArchiveFileExitsOnQuit(t *testing.T) {
+	archive := NewChirpArchiveConfig("fake-directory-path")
+	writer := NewMockArchiveWriter()
+	exit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-exit:
+				return
+			case <-time.After(3 * time.Second):
+				panic("timeout: archive.WriteFile did not exit on Quit()!")
+			}
+		}
+	}()
+
+	close(writer.Quit())
+	archive.WriteFile(writer)
+	exit <-true
+}
+
+
 type MockArchiveErrorWriter struct {
 	*MockArchiveWriter
 }